c724aea27cfbf6d171f6c4a5d639e26644c1e954
Kyu Lee <kwooler@gmail.com>
2023-03-26 22:01:39 -0700

    L55

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-def printscores():
-    # Make your code
+def printscores1(**scores):
+    for k, v in scores.items():
+        print(f'Subject {k:>10}: {v:>10}')
 
-    # make a function call 'printscore( )'
+
+def printscores2(scores):
+    for k, v in scores.items():
+        print(f'Subject {k:>10}: {v:>10}')
+
+
+def printscores3(Computer, Math, English):
+    print(f'{Math:>10} {English:>10} {Computer:>10}')
+
+
+def main():
+    kwargs = {'Math': 90, 'English': 100, 'Computer': 90}
+    printscores1(**kwargs)
+    printscores2(kwargs)
+    printscores3(**kwargs)
+
+
+if __name__ == '__main__':
+    main()
--- main_test.py
+++ main_test.py
@@ -3,10 +3,11 @@ import main
 
 
 def test_main():
-    
-    kargs = { 'Math': 90, 'English':100, 'Computer': 90}
-    main.printscores(**kargs)
-    
+
+    kargs = {'Math': 90, 'English': 100, 'Computer': 90}
+    main.printscores1(**kargs)
+    main.printscores2(kargs)
+    main.printscores3(**kargs)
+
     # assert v1 == -10, "Min value does not match"
     # assert v2 == 5, "Max value does not match"
-    
