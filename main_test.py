fe8871c1594bd8eaee2075c27026ddf559a8db9c
Kyu Lee <kwooler@gmail.com>
2023-05-24 22:48:56 -0700

    L55

--- main_test.py
+++ main_test.py
@@ -5,9 +5,9 @@ import main
 def test_main():
 
     kargs = {'Math': 90, 'English': 100, 'Computer': 90}
-    main.printscores1(**kargs)
-    main.printscores2(kargs)
-    main.printscores3(**kargs)
+    assert main.printscores1(**kargs) == None
+    assert main.printscores2(kargs) == None
+    assert main.printscores3(**kargs) == None
 
     # assert v1 == -10, "Min value does not match"
     # assert v2 == 5, "Max value does not match"
